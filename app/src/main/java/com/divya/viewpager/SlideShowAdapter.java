725ee21ceace3d1af2290dce3676253c2098b7c3
Divya Gupta <divagupta2392@gmail.com>
2017-09-19 12:44:32 +0530

    Initial commit

---
+++ app/src/main/java/com/divya/viewpager/SlideShowAdapter.java
@@ -0,0 +1,69 @@
+package com.divya.viewpager;
+
+import android.content.Context;
+import android.media.Image;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+/**
+ * Created by divya on 19/09/17.
+ */
+
+public class SlideShowAdapter extends PagerAdapter {
+
+    private Context context;
+    LayoutInflater inflater;
+
+    public int[] images = {
+            R.mipmap.ic_launcher,
+            R.mipmap.ic_launcher,
+            R.mipmap.ic_launcher,
+            R.mipmap.ic_launcher,
+            R.mipmap.ic_launcher,
+            R.mipmap.ic_launcher,
+            R.mipmap.ic_launcher,
+            R.mipmap.ic_launcher,
+            R.mipmap.ic_launcher
+    };
+
+    public SlideShowAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+
+        return images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return (view == (LinearLayout)object);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View view = inflater.inflate(R.layout.slidshow_layout,container,false);
+
+        ImageView image = (ImageView)view.findViewById(R.id.imageView_id);
+        image.setImageResource(images[position]);
+        container.addView(view);
+
+        return view;
+
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+
+        container.removeView((LinearLayout)object);
+
+    }
+}
